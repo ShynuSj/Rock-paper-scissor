a7d50f9818c087c290858822b721c9758493b412
shynu jose <100471775+ShynuSj@users.noreply.github.com>
2022-04-19 12:47:01 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,21 +1,26 @@
-import  random
+import random
 
 print('WELCOME TO ROCK, PAPER, SCISSOR GAME\n')
-user_choice = ['r','p','s']
-system = ('rock','paper','scissor')
-play_again = ('y','n')
+user_choice = ['r', 'p', 's']
+system = ('rock', 'paper', 'scissor')
+play_again = ('y', 'n')
+
 
 def start():
     user = None
     while user not in user_choice:
-        user = input('rock = r, paper = p or scissor = s?: ').lower()
+        user = input('rock, paper or scissor?: ').lower()
 
     system_choice = random.choice((system))
     print(f'you choosed = {user} \ncomputer choosed = {system_choice}')
 
-
-    if (user == 'r' and system == 'scissor') or (user == 'p' and system == 'rock') or (user == 's' and system == 'paper'):
+    if (user == 'r' and system_choice == 'scissor') or (user == 'p' and system_choice == 'rock') or (
+            user == 's' and system_choice == 'rock'):
         print('you won')
+
+    elif (user == 'r' and system_choice == 'rock') or (user == 'p' and system_choice == 'paper') or (
+            user == 's' and system_choice == 'scissor'):
+        print('Thats a Tie')
     else:
         print('you lose')
 
@@ -28,5 +33,6 @@ def start():
 
         else:
             break
+
 
 start()
